807682cc4d6d9e099f37033582a53b7109200b0d
Deoclecio Filho <deoclecio@ice.ufjf.com>
2019-09-09 19:02:15 -0300

    Teclas de ação

--- Script.js
+++ Script.js
@@ -1,16 +1,26 @@
-// Variáveis
+(function(){
+// Canvas
 var canvas = document.querySelector("canvas");
 canvas.width = 400;
-canvas.height = 600;
+canvas.height = 500;
 canvas.style = "border: 1px solid";
 var ctx = canvas.getContext("2d");
 
+// Variáveis
 var teclas = {
     espaco: 0,
     esquerda: 0,
     cima: 0,
     direita: 0,
     baixo: 0
+}
+
+var jogo = {
+    carregando : 0,
+    inicio : 1,
+    pausa: 2,
+    fim : 3,
+    estado : Carregando
 }
 
 //Funções
@@ -60,9 +70,15 @@ addEventListener("keyup", function (e) {
         case 40:
             teclas.baixo = 0;
             break;
+        case ENTER:
+            if (jogo.estado !== jogo.inicio)
+                jogo.estado = inicio;
+            else
+                jogo.estado = jogo.pausa;
         default:
             break;
     }
 
 });
    
+}());
